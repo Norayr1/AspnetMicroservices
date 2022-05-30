3f86bc8ac578d5e0149028ad9279e78eb08347f8
Norayr Sargsyan <norayr.sargsyan@technamin.com>
2022-05-30 14:33:22 +0400

    hot fix

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -25,7 +25,7 @@ namespace Catalog.API.Controllers
 
 
         [HttpGet]
-        [ProducesResponseType(typeof(IEquatable<Product>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
             var products = await _repository.GetProducts();
